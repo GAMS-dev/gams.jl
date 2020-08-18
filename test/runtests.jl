0e80f337e1b906e276a4079ac121b63aac6919ad
Renke Kuhlmann <rkuhlmann@gams.com>
2020-08-18 10:14:39 +0200

    added tests based on gamslib using convert

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,14 @@
+
+println("")
+println("-"^30)
+println("MathOptInterface Tests")
+println("-"^30)
+println("")
 include("MOI_wrapper.jl")
+
+println("")
+println("-"^30)
+println("GAMS Model Library Tests")
+println("-"^30)
+println("")
+include("gamslib.jl")
